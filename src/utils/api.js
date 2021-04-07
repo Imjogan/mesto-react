6e35c9fbe855d653939b9d349ffea4109337981b
Ivan Minin <wow.mjogan.official@yandex.ru>
2021-04-07 13:54:34 +0300

    вернул api.js

---
+++ src/utils/api.js
@@ -0,0 +1,155 @@
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+  
+  // получаем информацию о карточках
+  getInitialCards() {
+    return fetch(this._baseUrl+'/cards', {
+      method: 'GET',
+      headers: this._headers
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  // получаем информацию о пользователе
+  getUserInfo() {
+    return fetch(this._baseUrl+'/users/me', {
+      method: 'GET',
+      headers: this._headers
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  getInitialData() {
+    return Promise.all([this.getInitialCards(), this.getUserInfo()]);
+  }
+
+  // отправляем информацию о пользователе
+  setUserInfo(name, status) {
+    return fetch(this._baseUrl+'/users/me', {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: status
+      })
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  // создаем карточку
+  createUserInfo(name, link) {
+    return fetch(this._baseUrl+'/cards', {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  // удаляем карточку
+  deleteCard(cardID) {
+    return fetch(this._baseUrl+'/cards/'+cardID, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  // ставим лайк на карточку
+  setLike(cardID) {
+    return fetch(this._baseUrl+'/cards/likes/'+cardID, {
+      method: 'PUT',
+      headers: this._headers
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  // ставим лайк на карточку
+  deleteLike(cardID) {
+    return fetch(this._baseUrl+'/cards/likes/'+cardID, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  // обновляем аватар
+  updateAvatar(avatarUrl) {
+    return fetch(this._baseUrl+'/users/me/avatar', {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: avatarUrl
+      })
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // если ошибка, отклоняем промис
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+}
+
+// создаем класс для связи с сервером
+const api = new Api({
+  // базовый адрес обращения
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-21',
+  headers: {
+    // уникальный токен пользователя
+    authorization: 'a3ab0050-d01a-4f5a-9bb4-4a039b0aa641',
+    // MIME Type - формат отправляемых данных (формат JSON)
+    'Content-Type': 'application/json'
+  }
+});
+
+export default api;
